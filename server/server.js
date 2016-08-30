3e681c5ff66aa04c4447ccf0a1adf234596985d9
Jones Prabu <jonesprabu@gmail.com>
2016-08-30 17:13:08 +0530

    Commit

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ var boot = require('loopback-boot');
 
 var app = module.exports = loopback();
 
+app.use(loopback.token({ model: app.models.accessToken, currentUserLiteral: 'me' }));
+
 app.start = function() {
   // start the web server
   return app.listen(function() {
